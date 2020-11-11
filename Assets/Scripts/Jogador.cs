948924ac5f692e7cbcc5b354f4dea9cbfb3af479
Paulo Salvatore <paulo@salvatore.tech>
2020-11-11 16:08:35 -0300

    Área de Jogo adicionada

--- Assets/Scripts/Jogador.cs
+++ Assets/Scripts/Jogador.cs
@@ -6,6 +6,8 @@ public class Jogador : MonoBehaviour
 {
     public float velocidade = 2f;
 
+    public BoxCollider2D areaJogo;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,8 +16,27 @@ public class Jogador : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Movimentação do Jogador
         var vertical = Input.GetAxis("Vertical");
+        var horizontal = Input.GetAxis("Horizontal");
 
-        transform.Translate(Vector2.up * vertical * velocidade * Time.deltaTime);
+        transform.Translate(new Vector2(horizontal, vertical) * velocidade * Time.deltaTime);
+
+        // Garantir que o jogador está dentro da área de jogo
+
+        var position = areaJogo.transform.position;
+        var extents = areaJogo.bounds.extents;
+        var offset = areaJogo.offset;
+
+        var limiteXMin = -extents.x + position.x + offset.x * 2.5f;
+        var limiteXMax = extents.x + position.x + offset.x * 2.5f;
+
+        var limiteYMin = -extents.y + position.y + offset.y * 2.5f;
+        var limiteYMax = extents.y + position.y + offset.y * 2.5f;
+
+        transform.position = new Vector2(
+            Mathf.Clamp(transform.position.x, limiteXMin, limiteXMax),
+            Mathf.Clamp(transform.position.y, limiteYMin, limiteYMax)
+        );
     }
 }
