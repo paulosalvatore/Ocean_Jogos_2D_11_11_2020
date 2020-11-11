a13f826ec811a624ed9eb0071523387c53fcd2ea
Paulo Salvatore <paulo@salvatore.tech>
2020-11-11 15:30:13 -0300

    Movimentação do Jogador

--- Assets/Scripts/Jogador.cs
+++ Assets/Scripts/Jogador.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Jogador : MonoBehaviour
 {
+    public float velocidade = 2f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var vertical = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector2.up * vertical * velocidade * Time.deltaTime);
     }
 }
