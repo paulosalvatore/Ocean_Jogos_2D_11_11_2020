440b2791b6af8a917ebaafbe33cf39bea4ede6a5
Paulo Salvatore <paulo@salvatore.tech>
2020-11-16 14:55:14 -0300

    Prefab da explosão

---
+++ Assets/Scripts/Explosao.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosao : MonoBehaviour
+{
+    [Range(0f, 10f)]
+    public float delayDestruir = 1f;
+
+    private void Start()
+    {
+        Destroy(gameObject, delayDestruir);
+    }
+}
